52d3d1c4d073c3fd9973cdbe7ad7881d19f9f1c7
Brian Flad <bflad417@gmail.com>
2014-04-07 19:57:35 -0400

    Fix my maintainer_email in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'confluence'
 maintainer 'Brian Flad'
-maintainer_email 'bflad@wharton.upenn.edu'
+maintainer_email 'bflad417@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures Atlassian Confluence'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
