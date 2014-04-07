aff979161439f0eefe2003c35a9787b50ad44111
Brian Flad <bflad417@gmail.com>
2014-04-07 19:40:33 -0400

    Update various root files

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ license           'Apache 2.0'
 description       'Installs/Configures Atlassian Confluence'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '1.7.1'
+
 recipe            'confluence', 'Installs/configures Atlassian Confluence'
 recipe            'confluence::apache2', 'Installs/configures Apache 2 as proxy (ports 80/443)'
 recipe            'confluence::database', 'Installs/configures MySQL/Postgres server, database, and user for Confluence'
