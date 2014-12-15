98ecdcf7e26892081f085c2a7f0dad2fcf0cb2d9
Emmanuel Sciara <emmanuel.sciara@gmail.com>
2014-12-16 00:59:20 +0100

    Corrected gem load pb on busser cucumber (not related to Chef 12 bug)

--- test/integration/confluence-installer-mysql-no-java/cucumber/spec_helper.rb
+++ test/integration/confluence-installer-mysql-no-java/cucumber/spec_helper.rb
@@ -1,13 +1,3 @@
-# require 'cucumber'
-
-# begin
-#   require 'rspec-expectations'
-# rescue LoadError
-#   require 'rubygems/dependency_installer'
-#   Gem::DependencyInstaller.new.install('rspec-expectations')
-#   require 'rspec-expectations'
-# end
-
 begin
   require 'faraday'
 rescue LoadError
