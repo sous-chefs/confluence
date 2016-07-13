29b01752d6b3f6e79251d0a6cce915edcb07d9a0
Mikhail Zholobov <legal90@gmail.com>
2016-07-13 17:39:26 +0300

    Bump version to 2.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'bflad417@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures Atlassian Confluence'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.0.0'
+version '2.1.0'
 
 recipe 'confluence', 'Installs/configures Atlassian Confluence'
 recipe 'confluence::apache2', 'Installs/configures Apache 2 as proxy (ports 80/443)'
