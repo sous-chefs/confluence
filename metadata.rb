5923efaa7ce138cf038f8d0493f203ca3b74a15f
Aaron C <aaron@digitalmediums.com>
2016-09-02 11:34:57 -1000

    update tempalte

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'legal90@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures Atlassian Confluence'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.1.1'
+version '2.1.2'
 
 issues_url 'https://github.com/parallels-cookbooks/confluence/issues'
 source_url 'https://github.com/parallels-cookbooks/confluence'
