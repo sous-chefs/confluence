c136966f870926f8b703afc0db774cea48748990
Mikhail Zholobov <legal90@gmail.com>
2016-11-15 17:19:47 +0200

    Use latest "postgresql" cookbook for integration tests
    We have to define addition attributes in the fixture cookbook to get Travis CI builds passed on RHEL/CentOS 6

--- test/fixtures/cookbooks/confluence_test/recipes/postgresql.rb
+++ test/fixtures/cookbooks/confluence_test/recipes/postgresql.rb
@@ -1,12 +1,21 @@
-node.set['confluence']['database']['type'] = 'postgresql'
-node.set['postgresql']['version'] = '9.3'
-node.set['postgresql']['password']['postgres'] = 'iloverandompasswordsbutthiswilldo'
+node.normal['confluence']['database']['type'] = 'postgresql'
+node.normal['postgresql']['version'] = '9.3'
+node.normal['postgresql']['password']['postgres'] = 'iloverandompasswordsbutthiswilldo'
 
 # For old platform versions PostgreSQL 9.3 is available in PGDG repos only
 if node['platform'] == 'ubuntu' && node['platform_version'].to_f <= 13.10
-  node.set['postgresql']['enable_pgdg_apt'] = true
+  node.normal['postgresql']['enable_pgdg_apt'] = true
+  node.normal['postgresql']['client']['packages'] = ['postgresql-client-9.3', 'libpq-dev']
+  node.normal['postgresql']['server']['packages'] = ['postgresql-9.3']
+  node.normal['postgresql']['contrib']['packages'] = ['postgresql-contrib-9.3']
+  node.normal['postgresql']['dir'] = '/etc/postgresql/9.3/main'
+  node.normal['postgresql']['server']['service_name'] = 'postgresql'
 elsif node['platform'] == 'centos' && node['platform_version'].to_f < 7.0
-  node.set['postgresql']['enable_pgdg_yum'] = true
+  node.normal['postgresql']['enable_pgdg_yum'] = true
+  node.normal['postgresql']['client']['packages'] = ['postgresql93', 'postgresql93-devel']
+  node.normal['postgresql']['server']['packages'] = ['postgresql93-server']
+  node.normal['postgresql']['contrib']['packages'] = ['postgresql93-contrib']
+  node.normal['postgresql']['server']['service_name'] = 'postgresql-9.3'
 end
 
 include_recipe 'confluence'
