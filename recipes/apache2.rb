d67b559785fd91e7f355420482522c091e6b4d37
Mikhail Zholobov <legal90@gmail.com>
2016-12-26 11:39:26 +0200

    Fix cookstyle offences

--- recipes/apache2.rb
+++ recipes/apache2.rb
@@ -20,12 +20,12 @@ if node['apache'].attribute?('listen_ports')
   # Compatibility with cookbook 'apache' < 3.2.0
   node.default['apache']['listen_ports'] |= [
     node['confluence']['apache2']['port'],
-    node['confluence']['apache2']['ssl']['port']
+    node['confluence']['apache2']['ssl']['port'],
   ]
 else
   node.default['apache']['listen'] |= [
     "*:#{node['confluence']['apache2']['port']}",
-    "*:#{node['confluence']['apache2']['ssl']['port']}"
+    "*:#{node['confluence']['apache2']['ssl']['port']}",
   ]
 end
 
