c8abc2330744b47bbe73ecf44f57ea20bff7e387
Martijn <martijn.niji@gmail.com>
2017-02-23 12:34:20 +0100

    Add workaround for https://jira.atlassian.com/browse/CONF-35722 (#148)
    Add workaround for https://jira.atlassian.com/browse/CONF-35722

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,6 +22,7 @@ default['confluence']['install_path'] = '/opt/atlassian/confluence'
 default['confluence']['install_type'] = 'installer'
 default['confluence']['user'] = 'confluence'
 default['confluence']['version'] = '6.0.4'
+default['confluence']['backup_when_update'] = true
 
 # Defaults are automatically selected from version via helper functions
 default['confluence']['url'] = nil
